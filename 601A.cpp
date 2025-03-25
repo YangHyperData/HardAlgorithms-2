15fd576364558bc367bef591b1a6080dbaaa5c79
YangStone <131573711+YangHyperData@users.noreply.github.com>
2025-03-25 14:28:31 +0700

    601A - The Two Routes

---
+++ 601A.cpp
@@ -0,0 +1,231 @@
+/* YangStone Come To Play */
+
+#include <vector>
+#include <bits/stdc++.h>
+#include <conio.h>
+#include <algorithm>
+using namespace std;
+
+#define RFOR(i, a, b) for (int(i) = (a) - 1; (i) >= (b); --(i))
+#define nl << '\n'
+#define vti vector<int>
+#define vtll vector<long long>
+#define yep cout << "YES" nl
+#define nope cout << "NO" nl
+#define ll long long
+#define lg long
+#define db double
+#define scanv(x)      \
+	for (auto &i : x) \
+	cin >> i
+// #define MOD 998244353
+#define dbg(x)         \
+	cerr << #x << " "; \
+	_print(x);         \
+	cerr << endl;
+#define pb push_back
+#define bg begin()
+#define ed end()
+#define rbg rbegin()
+#define red rend()
+#define dbg(x...)        \
+	cerr << #x << " : "; \
+	_print(x)
+#define uceil(a, b) ((a + b - 1) / (b))
+#define dbg(...)
+#define RFOR(i, a, b) for (int(i) = (a) - 1; (i) >= (b); --(i))
+#define sp << " "
+#define fr(i, l, r) for (int i = l; i <= r; i++)
+#define FOR(i, a, b) for (int(i) = (a); (i) < (b); ++(i))
+#define fl(i, r, l) for (int i = r; i >= l; i--)
+#define wh while
+#define fat(x, a) for (auto &x : a)
+#define srt(x) sort(x.bg, x.ed)
+#define printall(a)       \
+	for (auto &(i) : (a)) \
+	cout << i << ' '
+#define FOR(i, a, b) for (int(i) = (a); (i) < (b); ++(i))
+#define nlp(i, n) for (int i = 0; i < (int)(n); ++i)
+#define rsrt(x) sort(x.rbg, x.red)
+#define all(r) r.begin(), r.end()
+#define vi vector<int>
+// const int MAXN = 4e5 + 5;
+#define forn(i, n) for (int i = 0; i < int(n); ++i)
+#define rall(r) r.rbegin(), r.rend()
+#define fast_out()                    \
+	ios_base::sync_with_stdio(false); \
+	cin.tie(NULL)
+// const int mod = 1e9 + 7;
+// const int N = 1e5 + 10;
+const ll INF = 1e9 + 7;
+#define FORALL(i, a) for (auto &(i) : (a))
+#define yn(f) f ? cout << "YES\n" : cout << "NO\n"
+// void file() {freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);}
+int bin_pow(int a, int b, int mod) // luy thua nhi phan
+{
+	if (b == 0)
+		return 1;
+	int tmp = bin_pow(a, b / 2, mod);
+	if ((b & 1) == false)
+	{
+		return (tmp * tmp) % mod;
+	}
+	return (a * (tmp * tmp) % mod) % mod;
+}
+
+vector<int> factorize(int n)
+{
+	vector<int> res;
+	for (int i = 2; i * i <= n; i++)
+	{
+		while (n % i == 0)
+		{
+			res.push_back(i);
+			n /= i;
+		}
+	}
+	if (n != 1)
+		res.push_back(n);
+	return res;
+}
+//* Delete
+/*var a = document.querySelectorAll(".MJX_Assistive_MathML")
+a.forEach(s=> s.remove())
+*/
+bool compare(string &a, string &b)
+{
+	return a + b > b + a;
+}
+bool isPerfectSquare(ll n)
+{
+	ll temp = sqrt(n);
+
+	if (temp * temp == n)
+		return true;
+	return false;
+}
+bool check(char r)
+{
+	return (r == 'a' || r == 'e');
+}
+bool isPerfectSquare(int n)
+{
+	int rt = sqrt(n);
+	return (rt * rt == n);
+}
+bool isPrime(int n)
+{
+	for (int i = 2; i * i <= n; ++i)
+	{
+		if (n % i == 0)
+			return false;
+	}
+
+	return true;
+}
+ll func(ll n)
+{
+	ll sum = 0;
+	while (n > 0)
+	{
+		sum += n % 10;
+		n /= 10;
+	}
+	return sum;
+}
+const int mx = 401;
+typedef pair<int, int> pii;
+vector<pii> rail[mx];
+vector<pii> bus[mx];
+set<pii> s;
+int dist_rail[mx];
+int dist_bus[mx];
+bool vis[mx];
+void djkstra(vector<pii> adj[], int *dist)
+
+{
+	priority_queue<pii, vector<pii>, greater<pii>> pq;
+	pq.push(make_pair(0, 1));
+	dist[1] = 0;
+	while (!pq.empty())
+	{
+		int u = pq.top().second;
+		pq.pop();
+		if (vis[u])
+			continue;
+
+		vis[u] = true;
+		for (int i = 0; i < adj[u].size(); i++)
+		{
+			int v = adj[u][i].first;
+			int weight = adj[u][i].second;
+			if (dist[v] > dist[u] + weight)
+			{
+				dist[v] = dist[u] + weight;
+				pq.push(make_pair(dist[v], v));
+			}
+		}
+	}
+}
+void solve()
+{
+	int n, m, a, b;
+	cin >> n >> m;
+	while (m--)
+	{
+		cin >> a >> b;
+		rail[a].push_back(make_pair(b, 1));
+		rail[b].push_back(make_pair(a, 1));
+		s.insert(make_pair(min(a, b), max(a, b)));
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		dist_rail[i] = INT_MAX;
+		dist_bus[i] = INT_MAX;
+		vis[i] = false;
+	}
+	djkstra(rail, dist_rail);
+	for (int i = 1; i <= n; i++)
+	{
+		vis[i] = false;
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = i + 1; j <= n; j++)
+		{
+			pii p;
+			p.first = i;
+			p.second = j;
+			if (!s.count(p))
+			{
+				bus[i].push_back(make_pair(j, 1));
+				bus[j].push_back(make_pair(i, 1));
+			}
+		}
+	}
+	djkstra(bus, dist_bus);
+	if (dist_bus[n] == INT_MAX || dist_rail[n] == INT_MAX)
+	{
+		cout << "-1" << endl;
+	}
+	else
+	{
+		cout << max(dist_bus[n], dist_rail[n]) << endl;
+	}
+}
+
+int main()
+{
+	fast_out();
+	int t = 1;
+	// cin >> t;
+
+	wh(t--)
+	{
+
+		solve();
+		// cout << endl;
+	}
+
+	return 0;
+}
